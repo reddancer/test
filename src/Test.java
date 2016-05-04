92ee1fab2105b0e156d5a31ae1ee1b3a814b7865
liangyanzhi <liangyanhi@secoo.com>
2016-05-04 11:07:12 +0800

    modify

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hahhah--renshuoshanxihaofengguang-renshuole");
 		System.out.println("lalala");
+		System.out.println("hahah");
 
 	}
 
