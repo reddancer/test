c8111c5712922eba3d67293b93026f251309b6f4
liangyanzhi <liangyanhi@secoo.com>
2016-06-02 14:38:58 +0800

    justTest

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hahhah--renshuoshanxihaofengguang-renshuole");
-		System.out.println("lalala");
-		System.out.println("hahah");
+		Byte byte1 = 1;
+		
+		Byte byte2 = 1;
+		
+		System.out.println(byte1 == byte2);
 
 	}
 
